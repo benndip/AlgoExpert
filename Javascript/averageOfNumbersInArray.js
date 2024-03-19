32f560db797651c7507df60cf19704c6e1ebf7f5
obenatvivify <oben@vivifysolutions.net>
2024-03-19 02:15:19 +0100

    did more algorithms here

--- Javascript/averageOfNumbersInArray.js
+++ Javascript/averageOfNumbersInArray.js
@@ -4,7 +4,7 @@ const averageOfNumbers = (arr) => {
     for (let i = 0; i < arr.length; i++) {
         sum+= arr[i]; 
     }
-    return sum/arr.length
+    return sum/arr.length;
 }
 
 let numbers = [3,5,7,1,8,0,4]
